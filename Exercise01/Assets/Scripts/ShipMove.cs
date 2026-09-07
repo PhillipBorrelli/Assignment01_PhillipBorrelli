f54842ce18118fc93575bd19eab62e8c606149e1
PhillipBorrelli <borrel281@morrisville.edu>
2026-09-07 13:35:56 -0400

    In class examples
    learned more movement and physics

--- Exercise01/Assets/Scripts/ShipMove.cs
+++ Exercise01/Assets/Scripts/ShipMove.cs
@@ -1,17 +1,43 @@
+using UnityEditor.ShaderGraph.Internal;
 using UnityEngine;
 
-public class SimpleMovement : MonoBehaviour
+public class ShipMove : MonoBehaviour
 {
-    [SerializeField] float moveSpeed =3f;
+    [SerializeField] float moveeSpeed = 5f;
+    [SerializeField] float rotateSpeed = 120f;
     void Start()
     {
-        Debug.Log("Movement script started");
+
     }
+
     void Update()
     {
-        float moveX = Input.GetAxis("Horizontal");
-        transform.Translate(moveX * moveSpeed * Time.deltaTime, 0f, 0f);
-        
+        if (Input.GetKey(KeyCode.Q))
+        {
+            transform.Rotate(0f, 0f, rotateSpeed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.E))
+        {
+            transform.Rotate(0f, 0f, -rotateSpeed * Time.deltaTime);
+        }
+        float x = Input.GetAxis("Horizontal");
+        float y = Input.GetAxis("Vertical");
+        Vector3 Move = new Vector3(x, y, 0f);
+        //transform.Rotate(0f, 0.4f, 0f);
+        transform.Translate(Move * moveeSpeed * Time.deltaTime);
+
+    }
+
+        void OnCollisionEnter2D(Collision2D collision)
+        { 
+            Debug.Log("Collision happened " +collision.gameObject.name);
+        if (collision.collider.CompareTag("TEST"))
+        {
+            Debug.Log("HitObstacle");
+        }
     }
 }
+
+
+
 
